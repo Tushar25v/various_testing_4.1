94419a12793bb3ead5f53d38e6dd477fd9d4d990
Tushar25v <107396077+Tushar25v@users.noreply.github.com>
2025-07-01 14:54:10 +0530

    Update ttssss.py

--- ttssss.py
+++ ttssss.py
@@ -10,7 +10,7 @@ speech_file_path = Path(__file__).parent / "speech.mp3"
 with client.audio.speech.with_streaming_response.create(
     model="gpt-4o-mini-tts",
     voice="coral",
-    input="Tushar is the greatest of all time , dont bet against him pal, he may lose now but over long term he is 100 % winner ",
+    input="GOd is great !",
     instructions="Speak in a cheerful and positive tone.",
 ) as response:
     response.stream_to_file(speech_file_path)
